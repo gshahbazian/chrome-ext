1587e542f5b1a3979f6121f8c3b764870cfa1312
gshahbazian <gabeshahbazian@gmail.com>
2025-01-23 10:36:09 -0800

    Add pip shortcut

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
 chrome.commands.onCommand.addListener(async (command, tab) => {
   if (!tab) return
+  if (tab.url.startsWith('chrome://')) return
+
+  switch (command) {
+    case 'copy_tab_url':
+      copyCommand(tab)
+      break
+    case 'open_pip':
+      pipCommand(tab)
+      break
+  }
+})
+
+function copyCommand(tab) {
   if (!tab.url) return
 
   chrome.scripting.executeScript({
@@ -7,8 +20,16 @@ chrome.commands.onCommand.addListener(async (command, tab) => {
     func: copyToClipboard,
     args: [tab.url],
   })
-})
+}
 
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text)
+}
+
+function pipCommand(tab) {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id, allFrames: true },
+    world: "MAIN",
+    files: ["pip.js"],
+  })
 }
